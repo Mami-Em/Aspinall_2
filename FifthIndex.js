0ad31a962f1821c7e01ed7386846b1b65af1a40b
Mami-Em <100179286+Mami-Em@users.noreply.github.com>
2022-03-24 23:33:32 -0400

    Update FifthIndex.js

--- FifthIndex.js
+++ FifthIndex.js
@@ -20,42 +20,46 @@ export default function FifthIndex() {
     return (
         <div>
             {/* title */}
-            <div className="p-5">
-                <p className="font-serif text-4xl text-center pb-5">Our sites</p>
+            <div className="p-9 lg:p-12">
+                <p className="font-serif text-4xl text-center pb-12 sm:text-5xl">Our sites</p>
             </div>
-            {/* -> divide by 2 components*/}
-            <div>
-                <p className="px-5 text-3xl font-serif">
-                    We have 4 well established sites in 4 regions of Madagascar
-                </p>
-            </div>
-            {/* stop 2 components */}
-            {/* -> 4 regions */}
-
-            <div className="p-5 flex flex-col divide-y">
-                {region.map((item) => 
-                <a href={item.src} className="grid grid-cols-6 gap-5 items-center py-5 group">
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 p-4 rounded-full bg-gray-100 group-hover:bg-zinc-700 group-hover:text-gray-50 transition-100 duration-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                        </svg>
+            <div className="lg:bg-gray-50 lg:mx-12 lg:grid lg:grid-cols-2 lg:mb-12">
+                {/* lg */}
+                    {/* -> divide by 2 components*/}
+                    <div className="lg:flex lg:items-center">
+                        <p className="px-9 text-3xl font-serif sm:text-4xl sm:px-9">
+                            We have 4 well established sites in 4 regions of Madagascar
+                        </p>
                     </div>
-                    <div className="col-span-5">
-                        <p className="font-semibold">
-                            {item.title}
-                        </p>
-                        <p className="text-zinc-700">
-                            {item.description}
-                        </p>
-                    </div>  
-                </a>
-                      
-                )}
+                    {/* stop 2 components */}
+                    {/* -> 4 regions */}
+
+                    <div className="p-9 flex flex-col divide-y">
+                        {region.map((item) => 
+                        <a href={item.src} className="grid grid-cols-6 gap-5 items-center py-5 group sm:mx-12">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 p-4 rounded-full bg-gray-100 group-hover:bg-zinc-700 group-hover:text-gray-50 transition-100 duration-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                </svg>
+                            </div>
+                            <div className="col-span-5 sm:text-lg">
+                                <p className="font-semibold">
+                                    {item.title}
+                                </p>
+                                <p className="text-zinc-700">
+                                    {item.description}
+                                </p>
+                            </div>  
+                        </a>
+                            
+                        )}
+                    </div>
+                {/* end */}
             </div>
             
+            
             {/* -- break */}
-            <div className="p-8">
-            </div>
+            
             {/* -- next */}
         </div>
     )
